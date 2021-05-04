cde9a302569bf3f6d9f43e53e9dff88d6a2af4d0
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2021-05-04 16:43:28 -0300

    First Commit

---
+++ src/components/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async(category) =>{
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=43XCWoBx405vS3a57aBDpIqIe8lvnILP`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img =>{
+        return{
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return gifs;
+
+}
